0330932a32705a1ae3ab82b2d3cda05fca600a06
hbally <hbally@student.42.fr>
2019-03-08 17:23:00 +0100

    changed header

--- inc/lem_in.h
+++ inc/lem_in.h
@@ -3,14 +3,29 @@
 
 #include <stdint.h>
 
+/*
+** Macros
+*/
+
 # define OK 0
 # define KO 1
+
+/*
+** Generic list structure
+*/
 
 typedef struct		s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+/*
+** Sub-Components for node structure
+**	- gate_data : information about gate nodes
+**	- hop : next node in path for a given flow id
+**  - vector : node coordinates
+*/
 
 typedef struct		s_gate_data
 {
@@ -26,6 +41,16 @@ typedef struct		s_vector
 	int				y;
 }					t_vector;
 
+typedef struct		s_hop
+{
+	int				flow;
+	struct s_node	*hop_to;
+}					t_hop;
+
+/*
+** Main Node Component
+*/
+
 typedef struct		s_node
 {
 	uint8_t			type;
@@ -37,26 +62,33 @@ typedef struct		s_node
 	int				ant;
 }					t_node;
 
-typedef struct		s_hop
-{
-	int				flow;
-	t_node			*hop_to;
-}					t_hop;
+/*
+** Graph representation of input map
+*/
 
 typedef struct		s_graph
 {
 	t_node			*nodes;
 	t_node			*source;
 	t_node			*sink;
+	int				flow_max;
+	int				flow_best;
 }					t_graph;
+
+/*
+** Container for program data
+*/
 
 typedef struct		s_wrap
 {
 	t_list			*input_start;
 	t_list			*input_end;
 	t_graph			*graph;
-	int				best_flow;
 }					t_wrap;
+
+/*
+**	List Creation/Deletion
+*/
 
 void				add_start(t_wrap *wrap, void *content, t_list **start);
 void				add_end(t_wrap *wrap, void *content,
@@ -64,6 +96,19 @@ void				add_end(t_wrap *wrap, void *content,
 void				del_start(t_wrap *wrap, t_list **start);
 void				del_end(t_wrap *wrap, t_list **start, t_list **end);
 void				del_all(t_wrap *wrap, t_list **start);
+
+
+/*
+**	Create all possible flows and store them
+*/
+
+void		flow_create_all(t_wrap *wrap, t_graph *graph);
+t_list		*flow_find_new(t_wrap *wrap, t_node *source, t_node *sink);
+
+/*
+**	Collector for clean exit
+*/
+
 void				collector(t_wrap *wrap, uint8_t flag);
 
 #endif
