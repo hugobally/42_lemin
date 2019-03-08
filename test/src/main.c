1e26f1585f6a8b3e36a39c98ae0f1bdb2aeedbf5
hbally <hbally@student.42.fr>
2019-03-08 16:27:13 +0100

    added tests for list deletion

--- test/src/main.c
+++ test/src/main.c
@@ -16,18 +16,19 @@ int		main(void)
 {
 
 	/*
-	** SETUP
+	** VARIABLES
 	*/
 
 	t_wrap		wrap;
+	t_list 		*node;
 	char		*string;
-
-	/*
-	** LIST ADD START TEST
-	*/
-
 	int			i = 0;
 
+	/*
+	** LIST ADD START
+	*/
+
+	ft_printf("list add start test\n");
 	string = ft_strdup("teststring0");
 	while (i < 10)
 	{
@@ -36,7 +37,6 @@ int		main(void)
 		string[ft_strlen(string) - 1] += 1;
 		i++;
 	}
-	t_list *node;
 	node = wrap.input_start;
 	ft_printf("expected teststring9 > teststring0\n");
 	while (node)
@@ -44,16 +44,15 @@ int		main(void)
 		ft_printf("%s\n", (char*)node->content);
 		node = node->next;
 	}
-	ft_printf("list add start : done\n");
-
 	reset (&wrap);
+	ft_printf("------------------\n");
 
 	/*
-	** LIST ADD END TEST
+	** LIST ADD END
 	*/
 
+	ft_printf("list add end test\n");
 	i = 0;
-
 	string = ft_strdup("teststring0");
 	while (i < 10)
 	{
@@ -69,9 +68,79 @@ int		main(void)
 		ft_printf("%s\n", (char*)node->content);
 		node = node->next;
 	}
-	ft_printf("list add end : done\n");
 
-	reset (&wrap);
+	ft_printf("------------------\n");
+
+	/*
+	** LIST DEL START
+	*/
+
+	ft_printf("list del start test\n");
+	del_start(&wrap, &(wrap.input_start));
+	ft_printf("expected testring1 > teststring9\n");
+	node = wrap.input_start;
+	while (node)
+	{
+		ft_printf("%s\n", (char*)node->content);
+		node = node->next;
+	}
+	node = (t_list*)ft_memalloc(sizeof(t_list));
+	del_start(&wrap, &node);
+	if (node != NULL)
+	{
+		ft_printf("del list with 1 elem : KO\n");
+		print_error();
+	}
+	ft_printf("del list with 1 elem : OK\n");
+	ft_printf("------------------\n");
+
+	/*
+	** LIST DEL END
+	*/
+
+	ft_printf("list del end test\n");
+	del_end(&wrap, &(wrap.input_start), &(wrap.input_end));
+	ft_printf("expected testring1 > teststring8\n");
+	node = wrap.input_start;
+	while (node)
+	{
+		ft_printf("%s\n", (char*)node->content);
+		node = node->next;
+	}
+	node = (t_list*)ft_memalloc(sizeof(t_list));
+	del_end(&wrap, &node, &node);
+	if (node != NULL)
+	{
+		ft_printf("del list with 1 elem : KO\n");
+		print_error();
+	}
+	ft_printf("del list with 1 elem : OK\n");
+	ft_printf("------------------\n");
+
+	/*
+	** LIST DEL ALL
+	*/
+
+	ft_printf("list del all test\n");
+	del_all(&wrap, &(wrap.input_start));
+	ft_printf("expected null\n");
+	node = wrap.input_start;
+	if (node == NULL)
+		ft_printf("del all : OK\n");
+	while (node)
+	{
+		ft_printf("%s\n", (char*)node->content);
+		node = node->next;
+	}
+	node = (t_list*)ft_memalloc(sizeof(t_list));
+	del_all(&wrap, &node);
+	if (node != NULL)
+	{
+		ft_printf("del list with 1 elem : KO\n");
+		print_error();
+	}
+	ft_printf("del list with 1 elem : OK\n");
+	ft_printf("------------------\n");
 
 	return (0);
 }
