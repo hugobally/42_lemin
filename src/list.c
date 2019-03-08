28cc3dafdf9de74ddeb90b21b071682465b01380
hbally <hbally@student.42.fr>
2019-03-08 14:23:45 +0100

    added list.c

---
+++ src/list.c
@@ -0,0 +1,9 @@
+#include "lem_in.h"
+
+/*
+void					add_start(t_wrap *wrap, void *content, t_list **start)
+void					add_end(t_wrap *wrap, void *content, t_list **start)
+void					del_start(t_wrap *wrap, void *content, t_list **start)
+void					del_end(t_wrap *wrap, void *content, t_list **start)
+void					del_all(t_wrap *wrap, void *content, t_list **start)
+*/
