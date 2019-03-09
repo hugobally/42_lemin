3acabaf8e9ab37573f72485c74a1338da5c53603
hbally <hbally@student.42.fr>
2019-03-09 13:46:05 +0100

    WIP on flow_create_all.c

--- src/flow_create_all.c
+++ src/flow_create_all.c
@@ -1,14 +1,28 @@
 #include "lem_in.h"
 
-void		flow_create_all(t_wrap *wrap, t_graph *graph)
+void			flow_find_wrapper(t_wrap *wrap, t_graph *graph)
 {
-	t_list	*path;
+	graph->source->bfs_data.last_visited = graph->flow_max;
+	flow_find_new(wrap, graph->flow_max, graph->source);
+}
 
-	while ((path = flow_find_new(wrap, graph->source, graph->sink)))
+/*
+** Implementation of Edmonds-Karp [max flow using iterative BFS]
+** - find the next augmenting path using BFS (flow_find_new)
+** - update graph using new path in (flow_update_nodes)
+** - calculate and store length of new/updated paths (flow_update_gates)
+*/
+
+void			flow_create_all(t_wrap *wrap, t_graph *graph)
+{
+	graph->flow_max = 1;
+	flow_find_wrapper(wrap, graph);
+	while (wrap->bfs_output)
 	{
-		graph->flow_max++;
 //		flow_update_nodes(wrap, graph, path);
 //		flow_update_gates(wrap, graph);
-		del_all(wrap, &path);
+//		del bfs_output
+		graph->flow_max++;
+		flow_find_wrapper(wrap, graph);
 	}
 }
