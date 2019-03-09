6c1abe9f56f7c0774b5b575a949c68f589fc2aee
hbally <hbally@student.42.fr>
2019-03-09 19:27:28 +0100

    changed flow_create_all -- added debug

--- src/flow_create_all.c
+++ src/flow_create_all.c
@@ -1,5 +1,32 @@
 #include "lem_in.h"
 
+/*
+** DEBUG
+*/
+
+#include "libft.h"
+void		print_path(t_node *start)
+{
+	t_node	*node;
+
+	node = start;
+	while (node)
+	{
+		if (node->type != END)
+			ft_printf("%s -> ", node->name);
+		else
+			ft_printf("%s\n", node->name);
+		if (node->hop_data)
+			node = ((t_hop*)(node->hop_data->content))->hop_to;
+		else
+			node = NULL;
+	}
+}
+
+/*
+** Wrapper
+*/
+
 void			flow_find_wrapper(t_wrap *wrap, t_graph *graph)
 {
 	graph->source->bfs_data.last_visited = graph->flow_max;
@@ -15,13 +42,26 @@ void			flow_find_wrapper(t_wrap *wrap, t_graph *graph)
 
 void			flow_create_all(t_wrap *wrap, t_graph *graph)
 {
+
+	t_list *node;
 	graph->flow_max = 1;
 	flow_find_wrapper(wrap, graph);
 	while (wrap->bfs_output)
 	{
-//		flow_update_nodes(wrap, graph, path);
-//		flow_update_gates(wrap, graph);
-//		del bfs_output
+		flow_update_nodes(wrap, wrap->bfs_output, graph->flow_max);
+//		flow_update_gates(wrap, graph, graph->flow_max);
+		del_all(wrap, &(wrap->bfs_output));
+		//
+		//DEBUG 
+		node = graph->source->hop_data;
+		while (node)
+		{
+			print_path(((t_hop*)(node->content))->hop_to);
+			node = node->next;
+		}
+		ft_printf("-----\n");
+		//END DEBUG
+		//
 		graph->flow_max++;
 		flow_find_wrapper(wrap, graph);
 	}
