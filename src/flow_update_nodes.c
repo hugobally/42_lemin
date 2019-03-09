15044e94549c99137a5c963c44815019d2779f7d
hbally <hbally@student.42.fr>
2019-03-09 19:30:21 +0100

    added flow_update_nodes.c

---
+++ src/flow_update_nodes.c
@@ -0,0 +1,31 @@
+#include "libft.h"
+#include "lem_in.h"
+
+void			flow_update_nodes(t_wrap *wrap, t_list *path, int flow)
+{
+	t_list		*list_elem;
+	t_node		*node;
+	t_node		*next_node;
+	t_hop		*hop;
+
+	list_elem = path;
+	while (list_elem->next)
+	{
+		node = (t_node*)(list_elem->content);
+		next_node = (t_node*)(list_elem->next->content);
+		hop = (t_hop*)ft_memalloc(sizeof(t_hop));
+		hop->flow = flow;
+		if (next_node->hop_data
+				&& ((t_hop*)(next_node->hop_data->content))->hop_to == node)
+		{
+			hop->hop_to = NULL;
+			add_start(wrap, (void*)hop, &(next_node->hop_data));
+		}
+		else
+		{
+			hop->hop_to = next_node;
+			add_start(wrap, (void*)hop, &(node->hop_data));
+		}
+		list_elem = list_elem->next;
+	}
+}
