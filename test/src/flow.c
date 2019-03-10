20f06e6a87d1a60de25eca492e4f66644f8a1b7f
hbally <hbally@student.42.fr>
2019-03-10 17:24:27 +0100

    changed test

--- test/src/flow.c
+++ test/src/flow.c
@@ -5,13 +5,13 @@ void		flow_test(void)
 	t_wrap wrap;
 	t_node *nodes;
 //	t_list *node;
-//	t_hop *hop;
-	
+//	t_hop *hop; 
 	ft_bzero(&wrap, sizeof(t_wrap));
 	ft_bzero(&(wrap.graph), sizeof(t_graph));
 	
+	/*
+	//Basic split map
 	nodes = (t_node*)(ft_memalloc(sizeof(t_node) * 8));
-
 	wrap.graph.source = &(nodes[0]);
 	nodes[0].type = START;
 	nodes[0].name = ft_strdup("SOURCE");
@@ -37,6 +37,34 @@ void		flow_test(void)
 	add_start(&wrap, (void*)(&(nodes[5])), &(nodes[6].edges));
 	add_start(&wrap, (void*)(&(nodes[6])), &(nodes[5].edges));
 	add_start(&wrap, (void*)(&(nodes[7])), &(nodes[6].edges));
+	*/
+
+	//Crossing paths
+	nodes = (t_node*)(ft_memalloc(sizeof(t_node) * 7));
+	wrap.graph.source = &(nodes[0]);
+	nodes[0].type = START;
+	nodes[0].name = ft_strdup("SOURCE"); //S0
+	nodes[1].name = ft_strdup("A"); 	 //A1
+	nodes[2].name = ft_strdup("B"); 	 //B2
+	nodes[3].name = ft_strdup("C");		 //C3
+	nodes[4].name = ft_strdup("D");		 //D4
+	nodes[5].name = ft_strdup("E");		 //E5
+	nodes[6].name = ft_strdup("SINK");   //T6
+	nodes[6].type = END;
+					//             CHILD  <<<    PARENT
+	add_start(&wrap, (void*)(&(nodes[1])), &(nodes[0].edges));
+	add_start(&wrap, (void*)(&(nodes[2])), &(nodes[0].edges));
+	add_start(&wrap, (void*)(&(nodes[3])), &(nodes[1].edges));
+	add_start(&wrap, (void*)(&(nodes[1])), &(nodes[3].edges));
+	add_start(&wrap, (void*)(&(nodes[3])), &(nodes[2].edges));
+	add_start(&wrap, (void*)(&(nodes[2])), &(nodes[3].edges));
+	add_start(&wrap, (void*)(&(nodes[4])), &(nodes[3].edges));
+	add_start(&wrap, (void*)(&(nodes[3])), &(nodes[4].edges));
+	add_start(&wrap, (void*)(&(nodes[5])), &(nodes[3].edges));
+	add_start(&wrap, (void*)(&(nodes[3])), &(nodes[5].edges));
+	add_start(&wrap, (void*)(&(nodes[6])), &(nodes[4].edges));
+	add_start(&wrap, (void*)(&(nodes[6])), &(nodes[5].edges));
+
 
 	flow_create_all(&wrap, &(wrap.graph));
 	
@@ -46,38 +74,5 @@ void		flow_test(void)
 //		print_path(((t_hop*)(node->content))->hop_to);
 //		node = node->next;
 //	}
-
-	/*
-	** Simulate already existing flow
-	*/
-
-	/*
-	hop = (t_hop*)ft_memalloc(sizeof(t_hop));
-	hop->hop_to = &(nodes[2]);
-	add_start(&wrap, (void*)hop, &(nodes[0].hop_data));
-
-	hop = (t_hop*)ft_memalloc(sizeof(t_hop));
-	hop->hop_to = &(nodes[4]);
-	add_start(&wrap, (void*)hop, &(nodes[2].hop_data));
-
-	hop = (t_hop*)ft_memalloc(sizeof(t_hop));
-	hop->hop_to = &(nodes[7]);
-	add_start(&wrap, (void*)hop, &(nodes[4].hop_data));
-	*/
-
-	/*
-	** flow_find_new unit test
-	*/
-
-	/*
-	flow_find_new(&wrap, 1, &(nodes[0]));
-
-	node = wrap.bfs_output;
-	while (node)
-	{
-		ft_printf("%s\n", ((t_node*)(node->content))->name);
-		node = node->next;
-	}
-	*/
 
 }
