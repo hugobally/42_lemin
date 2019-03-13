8f74b6185dc92a6432ce5f43b05ac4358b813a14
hbally <hbally@student.42.fr>
2019-03-13 17:30:35 +0100

    changed main.c and added printing of output

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include "../inc/lem_in.h"
 #include "libft.h"
 
+void		printinput(t_list *input)
+{
+	t_list	*elem;
+
+	elem = input;
+	while (elem)
+	{
+		ft_printf("%s\n", ((char*)(elem->content)));
+		elem = elem->next;
+	}
+	ft_printf("\n");
+}
+
 int			main(void)
 {
 	t_wrap 	wrap;
@@ -9,8 +22,9 @@ int			main(void)
 	ft_bzero(&wrap, sizeof(t_wrap));
 	ft_bzero(&graph, sizeof(t_graph));
 	ft_read_map(&wrap, &graph);
-	ft_print_hash_tab(&(wrap.graph), 1);
-//	flow_create_all(&wrap, &(wrap.graph));
-//	output(&wrap, wrap.graph.source, wrap.graph.flow_best, wrap.graph.source_capacity);
+//	ft_print_hash_tab(&(wrap.graph), 1);
+	flow_create_all(&wrap, &(wrap.graph));
+	printinput(wrap.input_start);
+	output(&wrap, wrap.graph.source, wrap.graph.flow_best, wrap.graph.source_capacity);
 	return (0);
 }
