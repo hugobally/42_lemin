6a6244e63c4df5ec7b8b7d50ca0460713c2a8094
hbally <hbally@student.42.fr>
2019-03-17 22:17:19 +0100

    reorganized collector

--- src/collector.c
+++ src/collector.c
@@ -2,17 +2,19 @@
 #include "stdlib.h"
 #include "libft.h"
 
-void				collector(__attribute__((unused)) t_wrap *wrap, __attribute__((unused)) uint8_t flag)
+void			collector(t_wrap *wrap, uint8_t flag)
 {
 	if (flag == KO)
-		ft_putstr("ERROR\n");
-	if (flag == DONE)
 	{
-		//on resoud
-		//et on imprime
+		ft_printf("ERROR\n");
+		if (flag == DONE)
+		{
+			//on resoud
+			//et on imprime
+		}
+		ft_free_wrap(wrap);
+		exit(1);
 	}
-	ft_free_wrap(wrap);
-	if (flag == OK)
-		return ;
-	exit(1);
+	else
+		ft_free_wrap(wrap);
 }
