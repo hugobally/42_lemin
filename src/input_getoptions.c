511fa4d7ce6dd606d6adfd3520a593b2ab18c90d
hbally <hbally@student.42.fr>
2019-03-18 16:31:50 +0100

    added input_getoptions

--- src/input_getoptions.c
+++ src/input_getoptions.c
@@ -4,12 +4,12 @@
 
 static void		exit_usage(void)
 {
-	ft_printf("usage: lem-in [-el] < input.map\n\n");
+	ft_printf("usage: lem-in [-ev] < input.map\n\n");
 	ft_printf("-e : export data to .js in ./viz/data/\n");
 	ft_printf("     to launch vizualization, use 'open viz/index.html'\n");
 	ft_printf("\n");
-	ft_printf("-l : show the total number of lines printed on the\n");
-	ft_printf("     last line of resulting output\n");
+	ft_printf("-v : verbose output\n");
+	ft_printf("     prints additional information\n");
 	ft_printf("\n");
 	exit(EXIT_FAILURE);
 }
@@ -18,7 +18,7 @@ static void		add_option(const char c, t_opt *opt)
 {
 	int			i;
 
-	i = ft_strchri("el", c);
+	i = ft_strchri("ev", c);
 	if (i == -1)
 		exit_usage();
 	opt->opt_int = opt->opt_int | (1 << i);
@@ -42,5 +42,4 @@ void			get_options(t_opt *opt, int argc, char **argv)
 	}
 	if (i < argc)
 		exit_usage();
-	while (1);
 }
