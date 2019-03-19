1270d36298ada348040a25eb1e1cc1342fc16253
Adrien LAC <alac@e2r9p20.42.fr>
2019-03-19 14:43:49 +0100

    mise a la norme

--- inc/lem_in.h
+++ inc/lem_in.h
@@ -161,29 +161,29 @@ void				get_options(t_opt *opt, int argc, char **argv);
 **	Parsing
 */
 
-int     			ft_read_map(t_wrap *wraper, t_graph *graph);
-void	    		ft_make_tab(int size, t_wrap *wraper, t_graph *graph);
+int					ft_read_map(t_wrap *wraper, t_graph *graph);
+void				ft_make_tab(int size, t_wrap *wraper, t_graph *graph);
 void				ft_get_coord(char *str, t_node **node, t_wrap *wraper);
-void	     		ft_init_graph(t_wrap *wraper,
+void				ft_init_graph(t_wrap *wraper,
 									t_table *table,
 									t_graph *graph);
-void        		ft_make_node(t_wrap *wraper, char *str,
+void				ft_make_node(t_wrap *wraper, char *str,
 									t_graph *graph, int *type);
-int         		ft_make_graph(t_wrap *wraper, int size, t_graph *graph);
-int     			ft_nbr_size(int nbr);
-int                 ft_ants(char *line, t_wrap *wraper, int ants);
-unsigned long       hash(unsigned char *str);
-int     			ft_is_node(char *line, t_wrap *wraper);
+int					ft_make_graph(t_wrap *wraper, int size, t_graph *graph);
+int					ft_nbr_size(int nbr);
+int					ft_ants(char *line, t_wrap *wraper, int ants);
+unsigned long		hash(unsigned char *str);
+int					ft_is_node(char *line, t_wrap *wraper);
 int					ft_is_link(char *line, t_wrap *wraper, t_list **prec);
-int                 ft_is_comment(t_wrap *wraper, char *str, int past);
-void        		ft_get_edges(char **room_1, char **room_2,
+int					ft_is_comment(t_wrap *wraper, char *str, int past);
+void				ft_get_edges(char **room_1, char **room_2,
 									char *str, t_wrap *wraper);
 int					ft_add_edges(char **room_1, char **room_2, t_wrap *wraper,
 t_graph *graph);
-void       			ft_insert_in_tab(t_wrap *wraper, t_graph *graph,
+void				ft_insert_in_tab(t_wrap *wraper, t_graph *graph,
 									int index, t_node *new_node);
-void        		ft_print_hash_tab(t_graph *graph, int full);
-t_node      		*ft_get_hashed(char *str, t_graph *graph);
+void				ft_print_hash_tab(t_graph *graph, int full);
+t_node				*ft_get_hashed(char *str, t_graph *graph);
 void				ft_free_wrap(t_wrap *wrap);
 void				ft_free_hop(t_node *node);
 void				ft_mid_file(t_wrap *wraper, int flag, t_list **lst);
@@ -207,14 +207,14 @@ void				del_all(t_list **start);
 void				flow_create_all(t_wrap *wrap, t_graph *graph);
 void				flow_find_wrapper(t_wrap *wrap, t_graph *graph);
 void				flow_find_new(t_wrap *wrap, int flow, t_node *source);
-void				flow_update_nodes(t_wrap *wrap, t_list *path, int flow); 
+void				flow_update_nodes(t_wrap *wrap, t_list *path, int flow);
 void				flow_update_gates(t_wrap *wrap, t_node *source, int flow);
 
 /*
 ** 	Cost calculation for a given flow
 */
 
-t_gate_data 		*get_data(t_list *data_list, int flow);
+t_gate_data			*get_data(t_list *data_list, int flow);
 t_node				*get_best_gate(t_node *source, int flow);
 void				flow_simulate(t_graph *graph, t_node *source, int flow);
 
