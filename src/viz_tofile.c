e40ac1dea9094336a7d4efefc94fd901f34f2e4b
hbally <hbally@student.42.fr>
2019-03-19 16:56:26 +0100

    fixed delete_duplicate (previously commented out and not working)

--- src/viz_tofile.c
+++ src/viz_tofile.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   viz_tofile.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/19 16:24:02 by hbally            #+#    #+#             */
+/*   Updated: 2019/03/19 16:47:23 by hbally           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include "lem_in.h"
 #include "libft.h"
@@ -5,18 +17,14 @@
 int					create_file(t_wrap *wrap, char *path)
 {
 	int				fd;
-	
-	fd = open(path, O_RDWR | O_CREAT | O_TRUNC | O_SYMLINK, S_IRUSR | S_IWUSR);
+
+	fd = open(path, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		 return (0);
+		return (0);
 	if (wrap)
 		wrap->out_fd = fd;
 	return (1);
 }
-
-/*
-** Generic function to parse hash table
-*/
 
 void				parse_table(t_wrap *wrap, t_graph *graph, t_list **nodes,
 									void (*action)(t_wrap*, t_list*))
@@ -36,10 +44,6 @@ void				parse_table(t_wrap *wrap, t_graph *graph, t_list **nodes,
 	}
 }
 
-/*
-** Removes reverse edges to reduce the load on viz
-*/
-
 static uint8_t		find_reverse_edge(t_node *parent, t_node *child)
 {
 	t_list			*edge;
@@ -54,39 +58,39 @@ static uint8_t		find_reverse_edge(t_node *parent, t_node *child)
 	return (0);
 }
 
-void				delete_duplicate(__attribute__((unused)) t_wrap *wrap,
-										t_list *start)
+void				delete_duplicate(t_wrap *wrap, t_list *list)
 {
-	t_list			*elem;
 	t_node			*parent;
 	t_node			*child;
 	t_list			*edge;
+	t_list			*swap;
 
-	elem = start;
-	while (elem)
+	(void)wrap;
+	while (list)
 	{
-		parent = (t_node*)(elem->content);
+		parent = (t_node*)(list->content);
 		edge = parent->edges;
 		while (edge)
 		{
 			child = (t_node*)(edge->content);
 			if (find_reverse_edge(parent, child))
+			{
+				swap = edge->next;
 				del_one(edge, &(parent->edges));
-			edge = edge->next;
+				edge = swap;
+			}
+			else
+				edge = edge->next;
 		}
-		elem = elem->next;
+		list = list->next;
 	}
 }
-
-/*
-** Outputs lem_in data to .js data file for viz
-*/
 
 void				graph_to_file(t_wrap *wrap, t_graph *graph)
 {
 	ft_dprintf(wrap->out_fd, "var sourceCapacity = %d;\n\n",
 								graph->source_capacity);
-//	parse_table(wrap, graph, graph->nodes, &delete_duplicate);
+	parse_table(wrap, graph, graph->nodes, &delete_duplicate);
 	ft_dprintf(wrap->out_fd, "var nodes_data = [\n");
 	parse_table(wrap, graph, graph->nodes, &nodes_to_file);
 	ft_dprintf(wrap->out_fd, "]\n\n");
